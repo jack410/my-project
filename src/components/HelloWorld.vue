2b9949a11f3c0b8a3c59cb0860cc66b5a111b08d
jack410 <jack410@163.com>
2023-05-06 18:53:36 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input v-model="name" placeholder="请输入你的名字">
+    <p v-if="name">Hello, {{ name }}!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+    };
+  },
+};
+</script>
